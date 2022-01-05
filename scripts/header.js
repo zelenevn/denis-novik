e11bae64690e413b3fe3d9e39b05246f33d0c1dd
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2022-01-05 14:47:33 +0300

    change link color on scroll

--- scripts/header.js
+++ scripts/header.js
@@ -1,16 +1,32 @@
 
+const sections = document.querySelectorAll("section");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
 const links = document.querySelectorAll(".menu__link");
+let areas = [...sections, footer];
 const activeLinkClass = "menu__link_active";
 
 function changeActiveLink(target) {
-  links.forEach(element => {
-    element.classList.remove(activeLinkClass);
-  });
+  clearLinks();
   target.classList.add(activeLinkClass);
 };
 
-links.forEach(link => {
-  link.addEventListener("click", (event) => {
-    changeActiveLink(event.target);
+function clearLinks() {
+  links.forEach(link => {
+    link.classList.remove(activeLinkClass);
+  });
+}
+
+// links.forEach(link => {
+//   link.addEventListener("click", (event) => {
+//     changeActiveLink(event.target);
+//   });
+// });
+
+window.addEventListener("scroll", () => {
+  areas.forEach((area, index) => {
+    if (window.pageYOffset > area.offsetTop - window.innerHeight / 2 && window.pageYOffset < area.offsetTop + area.offsetHeight + window.innerHeight / 2) {
+      changeActiveLink(links[index]);
+    }
   });
 });
